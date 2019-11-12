db5675be1d83e08ba8c65aec8fb62a8911242c7b
HongLi <hongli921123@163.com>
2019-11-12 13:04:04 +0800

    提交代码分支

---
+++ git_demo/src/main/java/com/hongli/bbb.java
@@ -0,0 +1,14 @@
+package com.hongli;
+
+/**
+ * @ClassName bbb
+ * @Description
+ * @Author HL
+ * @Date 2019/11/12  13:03
+ * @Version 1.0
+ */
+public class bbb {
+    public static void main(String[] args) {
+        System.out.println("bbb");
+    }
+}
