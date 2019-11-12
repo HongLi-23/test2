99b968c8d9b65650cb4a83a81caf49cfc0811ccc
HongLi-23 <54502011+HongLi-23@users.noreply.github.com>
2019-11-12 12:53:28 +0800

    Update demo.java

--- git_demo/src/main/java/com/hongli/demo.java
+++ git_demo/src/main/java/com/hongli/demo.java
@@ -11,6 +11,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println(8888);
+        System.out.println(999);
 
     }
 }
