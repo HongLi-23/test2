f5bb177ba24897c9dc4f54e4a0b03f6615915c5f
HongLi <hongli921123@163.com>
2019-11-12 13:04:56 +0800

    提交代码分支aaa

---
+++ git_demo/src/main/java/com/hongli/aaa.java
@@ -0,0 +1,14 @@
+package com.hongli;
+
+/**
+ * @ClassName aaa
+ * @Description
+ * @Author HL
+ * @Date 2019/11/12  13:01
+ * @Version 1.0
+ */
+public class aaa {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
