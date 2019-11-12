31f4a03f9943eadd5e02ffe9ab1880bc46242b60
HongLi <hongli921123@163.com>
2019-11-12 12:43:24 +0800

    提交代码

---
+++ git_demo/src/main/java/com/hongli/demo.java
@@ -0,0 +1,15 @@
+package com.hongli;
+
+/**
+ * @ClassName demo
+ * @Description
+ * @Author HL
+ * @Date 2019/11/12  12:41
+ * @Version 1.0
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
+}
